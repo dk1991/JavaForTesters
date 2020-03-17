b5b25f7dc7d1452faf8de299e99f332ec11a6a8c
Dmitriy Kanaev <kanaev.dm@mail.ru>
2020-03-17 18:42:31 +0300

    add new methods

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,6 +2,26 @@ package ru.stqa.pft.sandbox;
 
 class MyFirstProgram {
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
+		hello("Oleg");
+		hello("Bob");
+
+		double l = 5;
+		System.out.println("Area = " + area(l));
+
+		double a = 4;
+		double b = 7;
+		System.out.println("Area = " + area(a, b));
+	}
+
+	public static void hello(String somebody) {
+		System.out.println("Hello, " + somebody + "!");
+	}
+
+	public static double area(double l) {
+		return l * l;
+	}
+
+	public static double area(double a, double b) {
+		return a * b;
 	}
 }	
